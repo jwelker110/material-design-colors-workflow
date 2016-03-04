b9fc79654af5dcbc78c0e8d9a1ee8e1ae7abf3dd
seteen <app.zenn@gmail.com>
2016-03-04 19:58:24 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Search Google Material Design Color
+
+This simple workflow lets you search Google Material Design Colors  
+
+Color list is here.  
+https://www.google.com/design/spec/style/color.html#color-color-palette
+
+## Copy the color code.
+Usage: `col {query}`
+
+After you hit `enter` the code of the selected color code will be pasted your front app and copied to your
+clipboard.
---
+++ material_design_color.rb
@@ -0,0 +1,44 @@
+require './color_to_hex'
+
+def item_xml(options = {})
+  <<-ITEM
+  <item arg="#{options[:arg]}" uid="#{options[:uid]}">
+    <title>#{options[:title]}</title>
+    <subtitle>#{options[:subtitle]}</subtitle>
+    <icon>#{options[:path]}</icon>
+  </item>
+  ITEM
+end
+
+images_path = File.expand_path('../images/colors', __FILE__)
+
+queries = ARGV.first.split(' ')
+
+colors = COLOR_TO_HEX.inject([]) do |res, (color_name, color_kinds)|
+  color_kinds.each do |color_num, color_val|
+    res << {
+      name: "#{color_name}_#{color_num}",
+      value: color_val
+    }
+  end
+  res
+end
+
+matched_colors = colors.select do |color|
+  queries.select do |query|
+    !color[:name].match(/#{query}/i)
+  end.length == 0
+end
+
+items = matched_colors.map do |color|
+  arg = "#{color[:value]}"
+  uid = "#{color[:name]}"
+  path = "images/colors/#{uid}.png"
+  title = "#{color[:name]}"
+  subtitle = "Copy #{arg} to clipboard"
+  item_xml({ arg: arg, uid: uid, path: path, title: title, subtitle: subtitle})
+end.join
+
+output = "<?xml version='1.0'?>\n<items>\n#{items}</items>"
+
+puts output
